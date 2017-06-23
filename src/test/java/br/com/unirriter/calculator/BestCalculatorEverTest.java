043f1ff1281b6ef6c53a0808c64713af34e1233b
Leonardo Sessegolo <leonardo.sessegolo@sap.com>
2017-06-23 16:32:19 -0300

    converting to gradle project

---
+++ src/test/java/br/com/unirriter/calculator/BestCalculatorEverTest.java
@@ -0,0 +1,13 @@
+package br.com.unirriter.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BestCalculatorEverTest {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+}
