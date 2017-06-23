89980c142fda31111239a7495e905819475694d8
Leonardo Sessegolo <leonardo.sessegolo@sap.com>
2017-06-23 17:11:14 -0300

    changing calculator code

--- src/main/java/br/com/unirriter/calculator/BestCalculatorEver.java
+++ src/main/java/br/com/unirriter/calculator/BestCalculatorEver.java
@@ -3,6 +3,14 @@ package br.com.unirriter.calculator;
 public class BestCalculatorEver {
 
 	public static int add(int a, int b) {
-		return a + b + 1;
+		return a + b;
+	}
+	
+	public static int multiple(int a, int b) {
+		return a * b;
+	}
+	
+	public static int subtraction(int a, int b) {
+		return a - b;
 	}
 }
--- src/test/java/br/com/unirriter/calculator/BestCalculatorEverTest.java
+++ src/test/java/br/com/unirriter/calculator/BestCalculatorEverTest.java
@@ -7,10 +7,18 @@ import org.junit.Test;
 public class BestCalculatorEverTest {
 	
 	@Test
-	public void testAddOnePlusOne() {
+	public void testAdd1Plus1() {
 		int a = 1;
 		int b = 1;
 		
 		assertEquals(2, BestCalculatorEver.add(a, b));
+	}
+	
+	@Test
+	public void testMultiple10By14() {
+		int a = 10;
+		int b = 14;
+		
+		assertEquals(140, BestCalculatorEver.multiple(a, b));
 	}
 }
